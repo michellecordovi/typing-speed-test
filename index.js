bfe76feabd8df3a71ccb6e9097516191e076b94d
Michelle Cordovi <michellecordovi@gmail.com>
2026-02-28 15:18:04 -0500

    create and update variables for character and word count, update on start typing

--- index.js
+++ index.js
@@ -11,15 +11,19 @@ fetch('./data.json')
   })
 .catch(err => console.error(err));
 
-//VARIABLES
+//DOCUMENT POINTER VARIABLES
 const startTypingWindow = document.querySelector('.start-typing-window'); //Start Typing window
 const startTypingBtn = document.querySelector('.start-typing-button'); //Start Typing button
 const passage = document.querySelector('.passage'); //PASSAGE
 const difficultyToggles = document.querySelectorAll('.difficulty-toggle');
 const modeToggles = document.querySelectorAll('.mode-toggle');
-let passageHidden = true; //Passage is hidden true or false
+
+//GAME VARIABLES
+let passageText = '';
 let difficulty = 'easy'; //difficulty variable - default is 'easy' when page starts
 let mode = 'timed'; //mode variable - default is 'timed' when page starts
+let characterCount;
+let wordCount;
 
 
 //TOGGLE DIFFICULTY
@@ -33,8 +37,6 @@ const selectDifficulty = (event) => {
 
   difficultyToggles.forEach(toggle => toggle.classList.remove('active'));
   btn.classList.add('active')
-
-  console.log(difficulty)
 };
 difficultyToggles.forEach(toggle => toggle.addEventListener('click', selectDifficulty));
 
@@ -49,18 +51,17 @@ const selectMode = (event) => {
 
   modeToggles.forEach(toggle => toggle.classList.remove('active'));
   btn.classList.add('active');
-  
-  console.log(mode);
 }
 modeToggles.forEach(toggle => toggle.addEventListener('click', selectMode));
 
 //Clicking Start Typing button will close start typing window and show passage
-const hideWindow = () =>{
+const startTyping = () =>{
   let randomIndex = Math.floor(Math.random() * passages[difficulty].length);
 
-  startTypingWindow.style.visibility = 'hidden';
-  passageHidden = false;
-  passage.innerHTML = passages[difficulty][randomIndex].text;//returns random passage based on difficulty selection
+  passageText = passages[difficulty][randomIndex].text;//assigns passageText a random passage based on difficulty selection
+  passage.innerHTML = passageText;//updates passage text on screen
+  startTypingWindow.style.visibility = 'hidden';//hides start typing window, shows passage
+  characterCount = passageText.length;//calculates character count
+  wordCount = passageText.split(' ').length;//calculates word count
 };
-
-startTypingBtn.onclick = hideWindow;
+startTypingBtn.onclick = startTyping;
