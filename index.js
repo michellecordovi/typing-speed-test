e35bdd9dd1a2f09a25f3f6723467a0be92b3cad1
Michelle Cordovi <michellecordovi@gmail.com>
2026-03-03 13:31:25 -0500

    adjust keypress function to account for backspace, adjust UI elements to account for backspacing

--- index.js
+++ index.js
@@ -1,133 +1,147 @@
 //fetches passages from json file
 let passages = {};
 
-fetch('./data.json')
-  .then(response => response.json())
-  .then(data => {
-    passages = data;
-    let randomI = Math.floor(Math.random()* passages.hard.length)
-    document.querySelector('.passage').innerHTML = passages.hard[randomI].text;
-  })
-.catch(err => console.error(err));
+fetch("./data.json")
+	.then((response) => response.json())
+	.then((data) => {
+		passages = data;
+		let randomI = Math.floor(Math.random() * passages.hard.length);
+		document.querySelector(".passage").innerHTML =
+			passages.hard[randomI].text;
+	})
+	.catch((err) => console.error(err));
 
 //DOCUMENT POINTER VARIABLES
-const startTypingWindow = document.querySelector('.start-typing-window'); //Start Typing window
-const startTypingBtn = document.querySelector('.start-typing-button'); //Start Typing button
-const passageWindow = document.querySelector('.passage'); //Passage container
-const difficultyToggles = document.querySelectorAll('.difficulty-toggle'); //
-const modeToggles = document.querySelectorAll('.mode-toggle');
+const startTypingWindow = document.querySelector(".start-typing-window"); //Start Typing window
+const startTypingBtn = document.querySelector(".start-typing-button"); //Start Typing button
+const passageWindow = document.querySelector(".passage"); //Passage container
+const difficultyToggles = document.querySelectorAll(".difficulty-toggle"); //
+const modeToggles = document.querySelectorAll(".mode-toggle");
 
 //GAME VARIABLES
 const gameState = {
-  passage: '',
-  difficulty: 'easy', //difficulty variable - default is 'easy' when page starts
-  mode: 'timed', //mode variable - default is 'timed' when page starts
-  characterCount: 0,
-  wordCount: 0,
-  passageIndex: 0,
-  currentCharacter: ''
+	passage: "",
+	difficulty: "easy", //difficulty variable - default is 'easy' when page starts
+	mode: "timed", //mode variable - default is 'timed' when page starts
+	characterCount: 0,
+	wordCount: 0,
+	passageIndex: 0,
+	currentCharacter: "",
 };
 
 //TOGGLE DIFFICULTY
 const selectDifficulty = (event) => {
-  let btn = event.target;
-  let selectedDifficulty = btn.dataset.difficulty;
+	let btn = event.target;
+	let selectedDifficulty = btn.dataset.difficulty;
 
-  if (selectedDifficulty === gameState.difficulty) return;
+	if (selectedDifficulty === gameState.difficulty) return;
 
-  gameState.difficulty = selectedDifficulty;
+	gameState.difficulty = selectedDifficulty;
 
-  difficultyToggles.forEach(toggle => toggle.classList.remove('active'));
-  btn.classList.add('active')
+	difficultyToggles.forEach((toggle) => toggle.classList.remove("active"));
+	btn.classList.add("active");
 };
-difficultyToggles.forEach(toggle => toggle.addEventListener('click', selectDifficulty));
+difficultyToggles.forEach((toggle) =>
+	toggle.addEventListener("click", selectDifficulty),
+);
 
 //TOGGLE MODE
 const selectMode = (event) => {
-  let btn= event.target;
-  let selectedMode = btn.dataset.mode;
+	let btn = event.target;
+	let selectedMode = btn.dataset.mode;
 
-  if(selectedMode === gameState.mode) return;
+	if (selectedMode === gameState.mode) return;
 
-  gameState.mode = selectedMode;
+	gameState.mode = selectedMode;
 
-  modeToggles.forEach(toggle => toggle.classList.remove('active'));
-  btn.classList.add('active');
-}
-modeToggles.forEach(toggle => toggle.addEventListener('click', selectMode));
-
+	modeToggles.forEach((toggle) => toggle.classList.remove("active"));
+	btn.classList.add("active");
+};
+modeToggles.forEach((toggle) => toggle.addEventListener("click", selectMode));
 
 //UI FUNCTIONS
 //make function that highlights the current character to be types
 const highlightCurrentCharacter = (character) => {
-  character.style.backgroundColor = 'var(--dark-gray)';
-  character.style.color = 'var(--white)';
-}
+	character.style.backgroundColor = "var(--dark-gray)";
+	character.style.color = "var(--white)";
+  character.style.textDecoration = 'none';
+};
+
 //make function to turn a character green if correctly input (this should remove the background from when it was highlighted as the current character)
 const turnCharacterGreen = (character) => {
-  character.style.color = 'green';
-  character.style.backgroundColor = 'var(--black)';
-}
+	character.style.color = "green";
+	character.style.backgroundColor = "var(--black)";
+  character.style.textDecoration = 'none';
+};
 //make function to turn a character red if incorrect
 const turnCharacterRed = (character) => {
-  character.style.color = 'red';
-  character.style.textDecoration = 'underline';
-  character.style.backgroundColor = 'var(--black)';
-}
-
+	character.style.color = "red";
+	character.style.textDecoration = "underline";
+	character.style.backgroundColor = "var(--black)";
+};
 
 //Clicking Start Typing button will close start typing window and show passage
-const startTyping = () =>{
-  let randomIndex = Math.floor(Math.random() * passages[gameState.difficulty].length);
-  let span;
-  
-  passageWindow.innerHTML = ''; //clears default passage places at document load
-  gameState.passage = passages[gameState.difficulty][randomIndex].text;//assigns passage a random passage based on difficulty selection
+const startTyping = () => {
+	let randomIndex = Math.floor(
+		Math.random() * passages[gameState.difficulty].length,
+	);
+	let span;
 
-  //creates a span element with the letter of each letter in the passage, adds this to the passage window
-  //this way, when typing, each letter's color can be individually changed
-  for(let i=0; i < gameState.passage.length; i++){
-    span = document.createElement('span');
-    span.classList.add('passage-char');
-    span.innerHTML = gameState.passage[i];
-    span.setAttribute('data-char', gameState.passage[i]); 
-    passageWindow.appendChild(span);
-  }
+	passageWindow.innerHTML = ""; //clears default passage places at document load
+	gameState.passage = passages[gameState.difficulty][randomIndex].text; //assigns passage a random passage based on difficulty selection
 
-  startTypingWindow.style.visibility = 'hidden';//hides start typing window, shows passage
-  gameState.characterCount = gameState.passage.length;//calculates character count
-  gameState.wordCount = gameState.passage.split(' ').length;//calculates word count
-  gameState.currentCharacter = passageWindow.children[gameState.passageIndex]//sets current character to first SPAN ELEMENT in the new passage
-  highlightCurrentCharacter(gameState.currentCharacter);
-  console.log(gameState.currentCharacter)
+	//creates a span element with the letter of each letter in the passage, adds this to the passage window
+	//this way, when typing, each letter's color can be individually changed
+	for (let i = 0; i < gameState.passage.length; i++) {
+		span = document.createElement("span");
+		span.classList.add("passage-char");
+		span.innerHTML = gameState.passage[i];
+		span.setAttribute("data-char", gameState.passage[i]);
+		passageWindow.appendChild(span);
+	}
+
+	startTypingWindow.style.visibility = "hidden"; //hides start typing window, shows passage
+	gameState.characterCount = gameState.passage.length; //calculates character count
+	gameState.wordCount = gameState.passage.split(" ").length; //calculates word count
+	gameState.currentCharacter = passageWindow.children[gameState.passageIndex]; //sets current character to first SPAN ELEMENT in the new passage
+	highlightCurrentCharacter(gameState.currentCharacter);
+	console.log(gameState.currentCharacter);
 };
 startTypingBtn.onclick = startTyping;
 
+//KEY PRESS EVENT LISTENER
 const keyPress = (event) => {
-  // Block control keys and only allow printable characters
-  if (event.key.length > 1 || event.ctrlKey || event.metaKey) {
+	// Block control keys and only allow printable characters, except backspace/delete
+	if (event.key === "Backspace" || event.key === "Delete") {
+    if(gameState.passageIndex > 0 ){
+      gameState.currentCharacter.style.backgroundColor = 'inherit'; //undoes highlight of previous character
+      gameState.currentCharacter.style.color = 'var(--light-gray)';
+		  gameState.passageIndex--; //changes passage index -1
+		  gameState.currentCharacter = passageWindow.children[gameState.passageIndex]; //updates next character based on index
+		highlightCurrentCharacter(gameState.currentCharacter);
+    }
     return;
-  }
+	} else if (event.key.length > 1 || event.ctrlKey || event.metaKey) {
+		return;
+	}
 
-  //normalizes all dashes to === hyphen '-'
-  const normalizeChar = (char) => {
-  return char.replace(/[–—]/g, '-'); // Convert en-dash and em-dash to hyphen
-  };
- 
-  const typedChar = normalizeChar(event.key);
-  const expectedChar = normalizeChar(gameState.currentCharacter.dataset.char);
- 
+	//normalizes all dashes to === hyphen '-'
+	const normalizeChar = (char) => {
+		return char.replace(/[–—]/g, "-"); // Convert en-dash and em-dash to hyphen
+	};
 
-  if(typedChar === expectedChar){
-    turnCharacterGreen(gameState.currentCharacter);
-  } else if (typedChar !== expectedChar){
-    turnCharacterRed(gameState.currentCharacter);
-  }
+	const typedChar = normalizeChar(event.key);
+	const expectedChar = normalizeChar(gameState.currentCharacter.dataset.char);
 
-  gameState.passageIndex++;//changes passage index
-  gameState.currentCharacter = passageWindow.children[gameState.passageIndex]//updates next character based on index
-  highlightCurrentCharacter(gameState.currentCharacter); //next character is highlighted 
+	if (typedChar === expectedChar) {
+		turnCharacterGreen(gameState.currentCharacter);
+	} else if (typedChar !== expectedChar) {
+		turnCharacterRed(gameState.currentCharacter);
+	}
 
+	gameState.passageIndex++; //changes passage index
+	gameState.currentCharacter = passageWindow.children[gameState.passageIndex]; //updates next character based on index
+	highlightCurrentCharacter(gameState.currentCharacter); //next character is highlighted
 };
 
-document.addEventListener('keypress', keyPress);
+document.addEventListener("keydown", keyPress);
